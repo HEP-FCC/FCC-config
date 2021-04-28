f5a1c8b9dc7100f044412b572e821564b399bc9b
clementhelsens <clement.helsens@cern.ch>
2021-04-28 09:11:24 +0200

    add tmp prod cards

---
+++ FCCee/Generator/Pythia8/p8_ee_Zqq_ecm240.cmd
@@ -0,0 +1,32 @@
+Random:setSeed = on
+Main:numberOfEvents = 1000         ! number of events to generate
+Main:timesAllowErrors = 5          ! how many aborts before run stops
+
+
+! 2) Settings related to output in init(), next() and stat().
+Init:showChangedSettings = on      ! list changed settings
+Init:showChangedParticleData = off ! list changed particle data
+Next:numberCount = 100            ! print message every n events
+Next:numberShowInfo = 1            ! print event information n times
+Next:numberShowProcess = 1         ! print process record n times
+Next:numberShowEvent = 0           ! print event record n times
+Stat:showPartonLevel = off
+
+! 3) Beam parameter settings. Values below agree with default ones.
+Beams:idA = 11                   ! first beam, e- = 11
+Beams:idB = -11                  ! second beam, e+ = -11
+
+! 4) Hard process : qq, at 365 GeV
+Beams:eCM = 240               ! CM energy of collision
+WeakSingleBoson:ffbar2ffbar(s:gmZ) = on
+
+PartonLevel:ISR = on               ! initial-state radiation
+PartonLevel:FSR = on               ! final-state radiation
+
+! Decays
+!Z0
+23:onMode = off
+23:onIfAny = 1 2 3 4 5 6
+!gamma
+22:onMode = off
+22:onIfAny = 1 2 3 4 5 6
