2f2be378123bdc3ffe27cdb731932f23f8305771
clementhelsens <clement.helsens@cern.ch>
2021-04-30 11:34:57 +0200

    BES and vtx for z pole

--- FCCee/Generator/Pythia8/p8_ee_Zuds_ecm91.cmd
+++ FCCee/Generator/Pythia8/p8_ee_Zuds_ecm91.cmd
@@ -11,7 +11,7 @@ Main:timesAllowErrors = 5          ! how many aborts before run stops
 ! 2) Settings related to output in init(), next() and stat().
 Init:showChangedSettings = on      ! list changed settings
 Init:showChangedParticleData = off ! list changed particle data
-Next:numberCount = 1000             ! print message every n events
+Next:numberCount = 10000             ! print message every n events
 Next:numberShowInfo = 1            ! print event information n times
 Next:numberShowProcess = 1         ! print process record n times
 Next:numberShowEvent = 0           ! print event record n times
