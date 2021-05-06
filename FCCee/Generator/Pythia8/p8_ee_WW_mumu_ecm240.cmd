9a0d55d875037177aea589c791292a61ed2a7532
Perez <Emmanuel.Perez@cern.ch>
2021-05-06 13:30:42 +0200

    Added p8_ee_WW_mumu_ecm240.cmd : decays W -> mu and W -> tau -> mu, proper mixture of decays

--- FCCee/Generator/Pythia8/p8_ee_WW_mumu_ecm240.cmd
+++ FCCee/Generator/Pythia8/p8_ee_WW_mumu_ecm240.cmd
@@ -3,12 +3,14 @@ Main:numberOfEvents = 1000         ! number of events to generate
 Main:timesAllowErrors = 5          ! how many aborts before run stops
 
 ! 2) Settings related to output in init(), next() and stat().
+Init:showOneParticleData = 24	   ! to check the decay channel numbers for W->mu and W->tau
 Init:showChangedSettings = on      ! list changed settings
 Init:showChangedParticleData = off ! list changed particle data
-Next:numberCount = 100             ! print message every n events
+Next:numberCount = 1000             ! print message every n events
 Next:numberShowInfo = 1            ! print event information n times
 Next:numberShowProcess = 1         ! print process record n times
-Next:numberShowEvent = 0           ! print event record n times
+Next:numberShowEvent = 0          ! print event record n times
+
 
 Beams:idA = 11                   ! first beam, e+ = 11
 Beams:idB = -11                   ! second beam, e- = -11
@@ -33,7 +35,34 @@ WeakDoubleBoson:ffbar2WW = on
 PartonLevel:ISR = on               ! no initial-state radiation
 PartonLevel:FSR = on               ! no final-state radiation
 
-! W decays to mu nu :
+! W decays to mu and to taus :
 24:onMode    = off                 ! switch off W boson decays
-24:onIfAny   = 13                  ! switch on W boson decay to muons
+24:onIfAny   = 13                  ! switch on W boson decay to muons or taus
+24:onIfAny   = 15
+
+! To ensure the proper mixture of W -> mu and W -> tau -> mu :
+! (the BRs are internally rescaled such that the sum is one)
+24:7:bRatio  = 1. 		! W -> mu
+24:8:bRatio  = 0.1739		! W  -> tau  x  tau -> mu
+24:7:meMode  = 100		! set meMode = 100 so that
+24:8:meMode  = 100              ! branching ratios are not overwritten at initialization
+24:0:bRatio  = 0 
+24:1:bRatio  = 0
+24:2:bRatio  = 0
+24:3:bRatio  = 0
+24:4:bRatio  = 0
+24:5:bRatio  = 0
+24:6:bRatio  = 0
+24:0:meMode  = 100
+24:1:meMode  = 100
+24:2:meMode  = 100
+24:3:meMode  = 100
+24:4:meMode  = 100
+24:5:meMode  = 100
+24:6:meMode  = 100
+
+
+! tau decays to muons only :
+15:onMode   = off
+15:onIfAny  = 13		!    tau  -> mu
 
