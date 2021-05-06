f548f9a5c5aac55d7b7206b460974fe38096dc45
Perez <Emmanuel.Perez@cern.ch>
2021-05-06 21:16:05 +0200

    Added ZH, ZZ and WW Pythia8 cards, 240 GeV, no BES

---
+++ FCCee/Generator/Pythia8/p8_noBES_ee_ZZ_ecm240.cmd
@@ -0,0 +1,30 @@
+Random:setSeed = on
+Main:numberOfEvents = 1000         ! number of events to generate
+Main:timesAllowErrors = 5          ! how many aborts before run stops
+
+! 2) Settings related to output in init(), next() and stat().
+Init:showChangedSettings = on      ! list changed settings
+Init:showChangedParticleData = off ! list changed particle data
+Next:numberCount = 100             ! print message every n events
+Next:numberShowInfo = 1            ! print event information n times
+Next:numberShowProcess = 1         ! print process record n times
+Next:numberShowEvent = 0           ! print event record n times
+
+Beams:idA = 11                   ! first beam, e+ = 11
+Beams:idB = -11                   ! second beam, e- = -11
+
+
+! Vertex smearing :
+Beams:allowVertexSpread = on
+Beams:sigmaVertexX = 9.70e-3   !  13.7 mum / sqrt2
+Beams:sigmaVertexY = 25.5E-6   !  36.1 nm / sqrt2
+Beams:sigmaVertexZ = 0.64      !  0.64 mm
+
+! 3) Hard process : ZZ at 240 GeV
+Beams:eCM = 240  ! CM energy of collision
+WeakDoubleBoson:ffbar2gmZgmZ = on
+
+! 4) Settings for the event generation process in the Pythia8 library.
+PartonLevel:ISR = on               ! no initial-state radiation
+PartonLevel:FSR = on               ! no final-state radiation
+
