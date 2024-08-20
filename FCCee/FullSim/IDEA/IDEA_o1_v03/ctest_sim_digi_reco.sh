172c2d1c356de393e182a835ff14a2f74310589a
BrieucF <brieucfrancois@hotmail.com>
2024-08-20 09:57:44 +0200

    [FullSim] Do not load key4hep if already present in the env

--- FCCee/FullSim/IDEA/IDEA_o1_v03/ctest_sim_digi_reco.sh
+++ FCCee/FullSim/IDEA/IDEA_o1_v03/ctest_sim_digi_reco.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# set-up the environment
-source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh || true # if the key4hep env is loaded twice, it returns a non zero exit code, '|| true' ensures it returns 0 to avoid stopping pipelines
+# set-up the Key4hep environment if not already set
+if [[ -z "${KEY4HEP_STACK}" ]]; then
+  source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
+else
+  echo "The Key4hep stack was already loaded in this environment."
+fi
 
 # run the SIM step
 ddsim --enableGun --gun.distribution uniform --gun.energy "10*GeV" --gun.particle e- --numberOfEvents 10 --outputFile IDEA_sim.root --random.enableEventSeed --random.seed 42 --compactFile $K4GEO/FCCee/IDEA/compact/IDEA_o1_v03/IDEA_o1_v03.xml
