f8197a4a6b25677fa53d7c6a7d1c02890ddc8bb1
Sanghyun Ko <sanghyun.ko@cern.ch>
2025-03-06 19:15:51 +0100

    replace DCH digitizer

--- FCCee/FullSim/IDEA/IDEA_o1_v03/ctest_sim_digi_reco.sh
+++ FCCee/FullSim/IDEA/IDEA_o1_v03/ctest_sim_digi_reco.sh
@@ -13,5 +13,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # run the SIM step
 ddsim --enableGun --gun.distribution uniform --gun.energy "10*GeV" --gun.particle e- --numberOfEvents 10 --outputFile IDEA_sim.root --random.enableEventSeed --random.seed 42 --compactFile $K4GEO/FCCee/IDEA/compact/IDEA_o1_v03/IDEA_o1_v03.xml --steeringFile $SCRIPT_DIR/SteeringFile_IDEA_o1_v03.py
 
+# download file for cluster counting technique
+DCHfilename="https://fccsw.web.cern.ch/fccsw/filesForSimDigiReco/IDEA/DataAlgFORGEANT.root"
+wget --no-clobber $DCHfilename
+
 # run the DIGI/RECO step
 k4run $SCRIPT_DIR/run_digi_reco.py
