dea77461932aee4222acb019323b973bec6d5445
Sanghyun Ko <sanghyun.ko@cern.ch>
2025-03-07 16:11:53 +0100

    reflecting the comment from DCH experts

--- FCCee/FullSim/IDEA/IDEA_o1_v03/run_digi_reco.py
+++ FCCee/FullSim/IDEA/IDEA_o1_v03/run_digi_reco.py
@@ -104,7 +104,7 @@ dch_digitizer = DCHdigi_v01("DCHdigi",
     fileDataAlg = "DataAlgFORGEANT.root",
     calculate_dndx = False, # cluster counting disabled (to be validated, see FCC-config#239)
     create_debug_histograms = True,
-    zResolution_mm = 1, # in mm
+    zResolution_mm = 30., # in mm - Note: At this point, the z resolution comes without the stereo measurement
     xyResolution_mm = 0.1 # in mm
 )
 
