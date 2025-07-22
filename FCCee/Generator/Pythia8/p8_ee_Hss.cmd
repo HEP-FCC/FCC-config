c8ccac86e61aa03b649d7f86dd111fc0b1d28dfa
Louis Portales <louis.portales@cern.ch>
2025-07-22 16:19:27 +0200

    Pythia8 cards for exclusive Higgs decays w/ inclusive lhe input

---
+++ FCCee/Generator/Pythia8/p8_ee_Hss.cmd
@@ -0,0 +1,41 @@
+
+! 1) Settings that will be used in a main program.
+Random:setSeed = on
+Main:timesAllowErrors = 5          ! how many aborts before run stops
+
+! 2) Settings related to output in init(), next() and stat() functions.
+Init:showChangedSettings = on      ! list changed settings
+Init:showAllSettings = off         ! list all settings
+Init:showChangedParticleData = on  ! list changed particle data
+Init:showAllParticleData = off     ! list all particle data
+Next:numberCount = 10              ! print message every n events
+Next:numberShowLHA = 10            ! print LHA information n times
+Next:numberShowInfo = 10           ! print event information n times
+Next:numberShowProcess = 10        ! print process record n times
+Next:numberShowEvent = 10          ! print event record n times
+Stat:showPartonLevel = off         ! additional statistics on MPI
+
+! 3) Tell Pythia that LHEF input is used
+Beams:frameType = 4
+Beams:setProductionScalesFromLHEF = off
+Beams:allowMomentumSpread  = off
+Beams:LHEF = LHE_OUT.LHE
+
+! Vertex smearing
+Beams:allowVertexSpread = on
+Beams:sigmaVertexX = 5.96e-3
+Beams:sigmaVertexY = 23.8E-6
+Beams:sigmaVertexZ = 0.397
+Beams:sigmaTime = 10.89 ! 36.3 ps
+
+! 4) Settings for the event generation process in the Pythia8 library.
+PartonLevel:ISR = on               ! initial-state radiation
+PartonLevel:FSR = on                ! final-state radiation
+
+Check:epTolErr = 1e-1               ! default 1e-4, necessary to allow BES
+LesHouches:matchInOut = off
+
+
+! 5) decay Higgs
+25:onMode = off
+25:onIfAny = 3
