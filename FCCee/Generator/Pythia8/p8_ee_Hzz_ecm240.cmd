f8b555c6db995b455486056efd17976aaf44f2bf
Louis Portales <louis.portales@cern.ch>
2025-07-22 17:09:45 +0200

    P8 H decays at different ecm

--- FCCee/Generator/Pythia8/p8_ee_Hzz_ecm240.cmd
+++ FCCee/Generator/Pythia8/p8_ee_Hzz_ecm240.cmd
@@ -23,10 +23,10 @@ Beams:LHEF = LHE_OUT.LHE
 
 ! Vertex smearing
 Beams:allowVertexSpread = on
-Beams:sigmaVertexX = 5.96e-3
-Beams:sigmaVertexY = 23.8E-6
-Beams:sigmaVertexZ = 0.397
-Beams:sigmaTime = 10.89 ! 36.3 ps
+Beams:sigmaVertexX = 9.70e-3   !  13.7 mum / sqrt2
+Beams:sigmaVertexY = 25.5E-6   !  36.1 nm / sqrt2
+Beams:sigmaVertexZ = 0.64      !  0.64 mm
+
 
 ! 4) Settings for the event generation process in the Pythia8 library.
 PartonLevel:ISR = on               ! initial-state radiation
@@ -38,4 +38,4 @@ LesHouches:matchInOut = off
 
 ! 5) decay Higgs
 25:onMode = off
-25:onIfAny = 13
+25:onIfAny = 23
