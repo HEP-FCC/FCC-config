743f592ffa886f460222caa58dacffd74cc2c1e6
SanghyunKo <sanghyun.ko@cern.ch>
2025-09-16 01:02:53 +0900

    fix merge conflict

--- FCCee/FullSim/IDEA/IDEA_o1_v03/run_digi_reco.py
+++ FCCee/FullSim/IDEA/IDEA_o1_v03/run_digi_reco.py
@@ -149,6 +149,142 @@ dNdxFromTracks = TrackdNdxDelphesBased("dNdxFromTracks",
                                   FillFactor=1.0,
                                   OutputLevel=ERROR)
 
+################ Dual-readout calorimeter
+# SiPM emulation
+from Configurables import SimulateSiPMwithEdep
+sipmEdep = SimulateSiPMwithEdep("SimulateSiPMwithEdep",
+    OutputLevel=DEBUG,
+    inputHitCollection = "DRcaloSiPMreadout_scint",
+    outputHitCollection = "DRcaloSiPMreadoutDigiHit_scint",
+    outputTimeStructCollection = "DRcaloSiPMreadoutDigiWaveform_scint",
+    readoutName = "DRcaloSiPMreadout",
+    # wavelength in nm (decreasing order)
+    wavelength = [
+        900., 850., 800., 750., 725.,
+        700., 675., 650., 625., 600.,
+        590., 580., 570., 560., 550.,
+        540., 530., 520., 510., 500.,
+        490., 480., 470., 460., 450.,
+        440., 430., 420., 400., 350.,
+        300.
+    ],
+    # Hamamatsu S14160-1310PS
+    sipmEfficiency = [
+        0.02, 0.025, 0.045, 0.06, 0.0675,
+        0.075, 0.0925, 0.11, 0.125, 0.14,
+        0.146, 0.152, 0.158, 0.164, 0.17,
+        0.173, 0.176, 0.178, 0.179, 0.18,
+        0.181, 0.182, 0.183, 0.184, 0.18,
+        0.173, 0.166, 0.158, 0.15, 0.12,
+        0.05
+    ],
+    # Kuraray SCSF-78
+    scintSpectrum = [
+        0., 0., 0., 0., 0.,
+        0., 0., 0.0003, 0.0008, 0.0032,
+        0.0057, 0.0084, 0.0153, 0.0234, 0.0343,
+        0.0604, 0.0927, 0.1398, 0.2105, 0.2903,
+        0.4122, 0.5518, 0.7086, 0.8678, 1.,
+        0.8676, 0.2311, 0.0033, 0.0012, 0.,
+        0.
+    ],
+    # Kuraray SCSF-78
+    absorptionLength = [
+        2.714, 3.619, 5.791, 4.343, 7.896,
+        5.429, 36.19, 17.37, 36.19, 5.429,
+        13., 14.5, 16., 18., 16.5,
+        17., 14., 16., 15., 14.5,
+        13., 12., 10., 8., 7.238,
+        4., 1.2, 0.5, 0.2, 0.2,
+        0.1
+    ],
+    # Kodak Wratten 9
+    filterEfficiency = [
+        0.903, 0.903, 0.903, 0.903, 0.903,
+        0.903, 0.902, 0.901, 0.898, 0.895,
+        0.893, 0.891, 0.888, 0.883, 0.87,
+        0.838, 0.76, 0.62, 0.488, 0.345,
+        0.207, 0.083, 0.018, 0., 0.,
+        0., 0., 0., 0., 0.,
+        0.
+    ],
+    # empirical value to keep (scint npe / ceren npe) =~ 5
+    scintYield = 0.565,
+    scaleADC = 0.000366 # Cheren scale ADC / 5
+)
+
+from Configurables import SimulateSiPMwithOpticalPhoton
+sipmOptical = SimulateSiPMwithOpticalPhoton("SimulateSiPMwithOpticalPhoton",
+    OutputLevel=DEBUG,
+    inputHitCollection = "DRcaloSiPMreadoutSimHit",
+    inputTimeStructCollection = "DRcaloSiPMreadoutTimeStruct",
+    inputWavlenCollection = "DRcaloSiPMreadoutWaveLen",
+    outputHitCollection = "DRcaloSiPMreadoutDigiHit",
+    outputTimeStructCollection = "DRcaloSiPMreadoutDigiWaveform",
+    # wavelength in nm (decreasing order)
+    wavelength = [
+        900., 850., 800., 750., 725.,
+        700., 675., 650., 625., 600.,
+        590., 580., 570., 560., 550.,
+        540., 530., 520., 510., 500.,
+        490., 480., 470., 460., 450.,
+        440., 430., 420., 400., 350.,
+        300.
+    ],
+    # Hamamatsu S14160-1310PS
+    sipmEfficiency = [
+        0.02, 0.025, 0.045, 0.06, 0.0675,
+        0.075, 0.0925, 0.11, 0.125, 0.14,
+        0.146, 0.152, 0.158, 0.164, 0.17,
+        0.173, 0.176, 0.178, 0.179, 0.18,
+        0.181, 0.182, 0.183, 0.184, 0.18,
+        0.173, 0.166, 0.158, 0.15, 0.12,
+        0.05
+    ],
+    scaleADC = 0.00178
+)
+
+# RNG for sipm emulation (TODO harmonize RNG with other modules)
+from Configurables import HepRndm__Engine_CLHEP__RanluxEngine_ as RndmEngine
+rndmEngine = RndmEngine('RndmGenSvc.Engine',
+  SetSingleton = True,
+  Seeds = [ 1234567 ] # default seed is 1234567
+)
+
+# DRC topoclustering
+# use const noise tool for the moment
+from Configurables import ConstNoiseTool
+constNoiseTool = ConstNoiseTool("ConstNoiseTool",
+    detectors = ["FiberDRCalo"],
+    systemEncoding = "system:5",
+    detectorsNoiseRMS = [0.001], # ad-hoc small value
+    detectorsNoiseOffset = [0.],
+    OutputLevel = INFO
+)
+
+from Configurables import CaloTopoClusterFCCee
+topoClusterAll = CaloTopoClusterFCCee("topoClusterAll",
+    cells = ["DRcaloSiPMreadoutDigiHit","DRcaloSiPMreadoutDigiHit_scint"],
+    clusters = "TopoClusterAll",
+    clusterCells = "TopoClusterAllCells",
+    useNeighborMap = False,
+    readoutName = "DRcaloSiPMreadout",
+    neigboursTool = None,
+    noiseTool = constNoiseTool,
+    systemEncoding = "system:5",
+    seedSigma = 4,
+    neighbourSigma = 2,
+    lastNeighbourSigma = 0,
+    calorimeterIDs=[25],
+    createClusterCellCollection=True,
+    OutputLevel = INFO
+)
+
+from Configurables import RndmGenSvc
+rndmGenSvc = RndmGenSvc("RndmGenSvc",
+  Engine = rndmEngine.name()
+)
+
 ################ Output
 io_svc.outputCommands = ["keep *"]
 
@@ -161,21 +297,32 @@ audsvc.Auditors = [chra]
 from k4FWCore import ApplicationMgr
 application_mgr = ApplicationMgr(
     TopAlg = [
-              vtxb_digitizer,
-              vtxd_digitizer,
-              siwrb_digitizer,
-              siwrd_digitizer,
-              dch_digitizer,
-              muon_digitizer,
-              tracksFromGenParticles, 
-              plotTrackDCHHitDistances,
-              dNdxFromTracks,
-              ],
+        vtxb_digitizer,
+        vtxd_digitizer,
+        siwrb_digitizer,
+        siwrd_digitizer,
+        dch_digitizer,
+        muon_digitizer,
+        tracksFromGenParticles,
+        plotTrackDCHHitDistances,
+        dNdxFromTracks,
+        sipmEdep,
+        sipmOptical,
+        topoClusterAll
+    ],
     EvtSel = 'NONE',
-    EvtMax   = -1,
-    ExtSvc = ['RndmGenSvc', root_hist_svc, EventDataSvc("EventDataSvc"), geoservice, audsvc, UniqueIDGenSvc("uidSvc")],
+    EvtMax = -1,
+    ExtSvc = [
+        root_hist_svc,
+        EventDataSvc("EventDataSvc"),
+        geoservice,
+        audsvc,
+        UniqueIDGenSvc("uidSvc"),
+        rndmEngine,
+        rndmGenSvc
+    ],
     StopOnSignal = True,
- )
+)
 
 for algo in application_mgr.TopAlg:
     algo.AuditExecute = True
