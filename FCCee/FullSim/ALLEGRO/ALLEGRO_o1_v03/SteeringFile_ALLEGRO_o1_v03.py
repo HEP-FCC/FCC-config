75e089a927676cd46be2f09f6d2ca29d88a7662f
andread3vita <andrea.de.vita@cern.ch>
2026-09-17 11:42:57 +0200

    Set shared flag to false if single thread

--- FCCee/FullSim/ALLEGRO/ALLEGRO_o1_v03/SteeringFile_ALLEGRO_o1_v03.py
+++ FCCee/FullSim/ALLEGRO/ALLEGRO_o1_v03/SteeringFile_ALLEGRO_o1_v03.py
@@ -13,3 +13,19 @@ SIM.outputFile = "ALLEGRO_o1_v03_sim.root"
 ## Keep all DCH hits
 SIM.filter.mapDetFilter["DCH_v2"] = "edep0"
 SIM.action.mapActions["DCH_v2"] = "Geant4TrackerAction"
+
+def Geant4Output2EDM4hep_DRC_plugin(dd4hepSimulation):
+    from DDG4 import EventAction, Kernel
+
+    shared = dd4hepSimulation.numberOfThreads > 1
+    evt_root = EventAction(
+        Kernel(), "Geant4Output2EDM4hep_DRC/" + dd4hepSimulation.outputFile, shared
+    )
+    evt_root.Control = True
+    output = dd4hepSimulation.outputFile
+    evt_root.Output = output
+    evt_root.enableUI()
+    Kernel().eventAction().add(evt_root)
+    return None
+
+SIM.outputConfig.userOutputPlugin = Geant4Output2EDM4hep_DRC_plugin
--- FCCee/FullSim/ALLEGRO/ALLEGRO_o2_v01/SteeringFile_ALLEGRO_o2_v01.py
+++ FCCee/FullSim/ALLEGRO/ALLEGRO_o2_v01/SteeringFile_ALLEGRO_o2_v01.py
@@ -13,3 +13,19 @@ SIM.outputFile = "allegro_o2_v01_sim.root"
 ## Keep all STT hits
 SIM.filter.mapDetFilter["STT_o1_v01"] = "edep0"
 SIM.action.mapActions["STT_o1_v01"] = "Geant4TrackerAction"
+
+def Geant4Output2EDM4hep_DRC_plugin(dd4hepSimulation):
+    from DDG4 import EventAction, Kernel
+
+    shared = dd4hepSimulation.numberOfThreads > 1
+    evt_root = EventAction(
+        Kernel(), "Geant4Output2EDM4hep_DRC/" + dd4hepSimulation.outputFile, shared
+    )
+    evt_root.Control = True
+    output = dd4hepSimulation.outputFile
+    evt_root.Output = output
+    evt_root.enableUI()
+    Kernel().eventAction().add(evt_root)
+    return None
+
+SIM.outputConfig.userOutputPlugin = Geant4Output2EDM4hep_DRC_plugin
