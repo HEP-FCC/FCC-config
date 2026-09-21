178f85b16b95323ffd24c8664560ee4a1fcc4b2e
andread3vita <andrea.de.vita@cern.ch>
2026-09-21 15:14:38 +0200

    Revert modifications for IDEA o2

--- FCCee/FullSim/IDEA/IDEA_o2_v01/SteeringFile_IDEA_o2_v01.py
+++ FCCee/FullSim/IDEA/IDEA_o2_v01/SteeringFile_IDEA_o2_v01.py
@@ -459,23 +459,6 @@ SIM.outputConfig.forceLCIO = False
 ##       SIM.outputConfig.myExtension = '.csv'
 ##
 
-def Geant4Output2ROOT_plugin(dd4hepSimulation):
-    from DDG4 import EventAction, Kernel
-
-    shared = dd4hepSimulation.numberOfThreads > 1
-    evt_root = EventAction(
-        Kernel(), "Geant4Output2ROOT/" + dd4hepSimulation.outputFile, shared
-    )
-    evt_root.Control = True
-    output = dd4hepSimulation.outputFile
-    evt_root.Output = output
-    evt_root.enableUI()
-    Kernel().eventAction().add(evt_root)
-
-    return None
-
-SIM.outputConfig.userOutputPlugin = Geant4Output2ROOT_plugin
-
 ################################################################################
 ## Configuration for the Particle Handler/ MCTruth treatment
 ################################################################################
