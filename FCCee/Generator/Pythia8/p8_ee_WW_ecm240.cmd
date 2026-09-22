99f62f2ca8196f79c520d85b21e6e6632d78c743
Juraj Smiesko <juraj.smiesko@cern.ch>
2026-09-22 14:50:15 +0200

    Add IDEA Delphes/EDM4hep cards and update ZH/ZZ/WW generator cards
    Copy `card_IDEA.tcl` and `edm4hep_IDEA.tcl` from the `pre_summer2026`
    branch, since `main` currently has no EDM4hep steering card for the
    IDEA detector. Also update `p8_ee_ZH_ecm240.cmd`, `p8_ee_ZZ_ecm240.cmd`
    and `p8_ee_WW_ecm240.cmd` to their newer `winter2023` versions.

--- FCCee/Generator/Pythia8/p8_ee_WW_ecm240.cmd
+++ FCCee/Generator/Pythia8/p8_ee_WW_ecm240.cmd
@@ -13,6 +13,15 @@ Next:numberShowEvent = 0           ! print event record n times
 Beams:idA = 11                   ! first beam, e+ = 11
 Beams:idB = -11                   ! second beam, e- = -11
 
+Beams:allowMomentumSpread  = off
+
+! Vertex smearing :
+Beams:allowVertexSpread = on
+Beams:sigmaVertexX = 9.80e-3   !  9.80 mum
+Beams:sigmaVertexY = 25.4E-6   !  25.4 nm
+Beams:sigmaVertexZ = 0.64      !  0.64 mm
+
+
 ! 3) Hard process : WW at 240 GeV
 Beams:eCM = 240  ! CM energy of collision
 WeakDoubleBoson:ffbar2WW = on
@@ -20,3 +29,8 @@ WeakDoubleBoson:ffbar2WW = on
 ! 4) Settings for the event generation process in the Pythia8 library.
 PartonLevel:ISR = on               ! no initial-state radiation
 PartonLevel:FSR = on               ! no final-state radiation
+
+ParticleDecays:limitCylinder = on
+ParticleDecays:xyMax = 2250.
+ParticleDecays:zMax = 2500.
+
